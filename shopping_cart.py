8238b8090e509d3a667cbf7917303c6f54f6d4fc
Marcel Ghisi <marcel.ghisi@gmail.com>
2024-06-17 13:22:15 +0200

    Fixing var not used

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,16 @@
 def CalculateTotal(cart):
     total = 0
-    total_items = len(cart)
     for item in cart:
-        total += item['price']
+        total += float(item['price'])
     return total
 
-def display_total(Total):
-    print("Total price: " + Total)
+def display_total(total):
+    print("Total price: $" + str(total))
 
 CART = [
     {'name': 'Item A', 'price': 10.99},
     {'name': 'Item B', 'price': 5.99},
-    {'name': 'Item C', 'price': '8.49'}
+    {'name': 'Item C', 'price': 8.49}
 ]
 
 for item in CART:
